e9f6f146507002e643aaf807f38db9d901cfbc67
pitkw <pitkwiecien@gmail.com>
2022-02-15 08:32:38 +0100

    A

---
+++ src/main/java/pl/piotrkwiecien/pizzaapplication/domain/model/OrderStatusType.java
@@ -0,0 +1,5 @@
+package pl.piotrkwiecien.pizzaapplication.domain.model;
+
+public enum OrderStatusType {
+    NEW, IN_PROGRESS, READY, OUT_FOR_DELIVERY, DELIVERED, CANCELLED
+}
---
+++ src/main/java/pl/piotrkwiecien/pizzaapplication/domain/model/SizeType.java
@@ -0,0 +1,5 @@
+package pl.piotrkwiecien.pizzaapplication.domain.model;
+
+public enum SizeType {
+    S, M, L
+}
