d49f666e44b80b9bc990c8f08b71a7b6e0c09c1e
uczen <pitkwiecien@gmail.com>
2022-03-15 09:17:26 +0100

    Added PizzaController.java

--- src/main/java/pl/piotrkwiecien/pizzaapplication/remote/rest/api/PizzaController.java
+++ src/main/java/pl/piotrkwiecien/pizzaapplication/remote/rest/api/PizzaController.java
@@ -1,4 +1,28 @@
 package pl.piotrkwiecien.pizzaapplication.remote.rest.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.piotrkwiecien.pizzaapplication.remote.rest.dto.request.UpdatePizzaDto;
+import pl.piotrkwiecien.pizzaapplication.remote.rest.dto.response.PizzaDto;
+import pl.piotrkwiecien.pizzaapplication.remote.rest.dto.request.AddPizzaDto;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RequestMapping(value = "/api/v1/pizzas", produces = APPLICATION_JSON_VALUE)
+@RestController
 public class PizzaController {
+    @PostMapping
+    public ResponseEntity<PizzaDto> addPizza(@RequestBody AddPizzaDto pizzaDto, @RequestHeader("Access-Token") String token){
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PizzaDto> updatePizza(@RequestBody UpdatePizzaDto pizzaDto, @PathVariable("id") Integer pizzaId, @RequestHeader("Access-Token") String token){
+        return ResponseEntity.ok(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePizza(@RequestHeader("Access-Token") String token){
+        return ResponseEntity.ok(null);
+    }
 }
